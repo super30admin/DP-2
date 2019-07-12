1aaa4e7b6976bcd31fa530f7263a5c9ce4dc3673
Arivoli AE <aehari2010@gmail.com>
2019-07-11 17:51:20 -0700

    Commit for Day 8 - Changes to Problem 2

--- CountCoinChange.java
+++ CountCoinChange.java
@@ -2,31 +2,27 @@ class CountCoinChange {
     public int change(int amount, int[] coins) {
         int[][] dp = new int[coins.length+1][amount+1];
 
-        //fill 1st column as 0
+        //fill 1st column as 1
         for(int i=0;i<dp.length;i++){
-            dp[i][0] = 0;
+            dp[i][0] = 1;
         }         
 
-        //fill first row as max
-        for(int j=1;j<dp[0].length;j++)
-            dp[0][j] = 9999;
-
         //calc dp array
-        // for(int i=1;i<dp.length;i++){
-        //     for(int j=1;j<dp[0].length;j++){
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
 
-        //         //copy from top
-        //         if(j < coins[i-1]){
-        //             dp[i][j] = dp[i-1][j];
-        //         }
-        //         //compare and calc
-        //         else{
-        //             dp[i][j] = Math.min(dp[i-1][j], dp[i][j - coins[i-1]] + 1);
-        //         }
+                //copy from top
+                if(j < coins[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }
+                //compare and calc
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]];
+                }
 
-        //         System.out.print(dp[i][j] + " ");
-        //     }
-        //     System.out.println();
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
 
                 
         }
@@ -35,7 +31,7 @@ class CountCoinChange {
 
 
 
-        return 0;   
+        return dp[dp.length-1][dp[0].length-1];   
     }
     public static void main(String[] args){
         System.out.println("Count Coin Change");
