7ae36b6e0af2c217ea15e443909cb692d5e14a5c
Rakshitha94 <rakshithalr3007@gmail.com>
2019-09-05 01:34:48 -0700

    Please Review

--- painthouses.py
+++ painthouses.py
@@ -1,16 +1,24 @@
+#time complexity: o(n2)
+#it ran on leetcode
+# no doubt
 
+#code explaination:
+#this is similar like pathsum.
+#we take the first row as red, green and blue
+#we traverse downward, apart from that coloumn and find the minimum among the other two elements  at the top and add
+# the minimum to the cuurent element
 def painthouse(matrix):
     lredhouse=matrix[0][0]
-    lgreenhouse=matrix[0][1]
+    lgreenhouse=matrix[0][1]        #taking the first row as red blue and green
     lbluehouse=matrix[0][2]
-    for i in range(1,len(matrix)):
-        currentred=min(lgreenhouse,lbluehouse)+matrix[i][0]
+    for i in range(1,len(matrix)): #we are traversing down
+        currentred=min(lgreenhouse,lbluehouse)+matrix[i][0]  #finding the minimum of other two coloumns and adding
         currentgreen=min(lbluehouse,lredhouse)+matrix[i][1]
         currentblue=min(lgreenhouse,lredhouse)+matrix[i][2]
-        lredhouse=currentred
+        lredhouse=currentred #making the current to the assigned value
         lbluehouse=currentblue
         lgreenhouse=currentgreen
-    return min(min(lgreenhouse,lbluehouse),lredhouse)
+    return min(min(lgreenhouse,lbluehouse),lredhouse) #finding the minimum of the last row
 
 
 if __name__ == '__main__':
