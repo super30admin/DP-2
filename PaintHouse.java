9e444c807a2d2ab0955922c9ebd7005213e84415
Shridhar Saurin Shah <42818678+Shridhar-2205@users.noreply.github.com>
2019-10-17 17:00:54 -0700

    Update PaintHouse.java

--- PaintHouse.java
+++ PaintHouse.java
@@ -1,1 +1,20 @@
+class Solution{
+public int minCost(int[][] costs) {
+    if(costs==null||costs.length==0)
+        return 0;
+ 
+    for(int i=1; i<costs.length; i++){
+        costs[i][0] += Math.min(costs[i-1][1], costs[i-1][2]);
+        costs[i][1] += Math.min(costs[i-1][0], costs[i-1][2]);
+        costs[i][2] += Math.min(costs[i-1][0], costs[i-1][1]);
+    }
+ 
+    int m = costs.length-1;
+    return Math.min(Math.min(costs[m][0], costs[m][1]), costs[m][2]);
+}
+}
 
+//SC:O(1)
+//TC:O(N)
+//accepted on leetcode
+//find minimum of each row add that value to the next rows excluding that columns row value, then take minimum of next row excluding the given column from previous row
