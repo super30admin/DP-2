d3a1e0e3daa7fb4b6d239e93ecb1c4fd5e9ef2d8
Anoop Martin <33829132+SuperStriker@users.noreply.github.com>
2019-10-21 18:28:31 -0700

    dp solution

---
+++ coin_change_2_dp_solution.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    public int change(int amount, int[] coins) {
+       
+    	int [][] dp  = new int [coins.length+1][amount+1];
+        int m= dp.length;
+        int n= dp[0].length;
+        dp[0][0]=1;
+        for(int i=1; i<m ; i++){
+            dp[i][0]=1;
+        for (int j=1 ; j< n ;  j++){
+            if(j< coins[i-1]){
+                dp[i][j]= dp[i-1][j];
+            } else{
+                dp[i][j]= dp[i-1][j] +dp[i][j-coins[i-1]];
+            }
+    }
+}
+    return dp[m-1][n-1];
+        
+    }
+}
