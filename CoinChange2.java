24c27454177c9f4a478405cef393355d8bb64b2f
Ajay Chauhan <chauhanajayvikram1992@gmail.com>
2019-11-29 10:09:22 -0800

    Coin change 2

--- CoinChange2.java
+++ CoinChange2.java
@@ -6,23 +6,25 @@ public class CoinChange2 {
 	//	LeetCode: yes
 	
 	public int change(int amount, int[] coins) {
-		if(coins == null) return 0;
+		// if(coins == null) return 1;
 		return helper(coins,0, amount);
 	}
 
 	private int helper(int[] coins, int i ,int amount){
-		if (amount < 0 || i > coins.length - 1) return 0;
+		
 		if (amount == 0) return 1;
+        if (amount < 0 ) return 0;
+        if( i >= coins.length) return 0;
 		// /case1 : chosen 
 		int case1 = helper(coins,i, amount - coins[i]);
 		//case2 not chosen
 		int case2 = helper(coins,i+1 ,amount);
-		int result = Math.min(case1, case2);
+//		int result = Math.min(case1, case2);
 
 		if(case1 == -1 ) return case2;
 		else  if(case2 == -1) return case1;
 		//returning sum of both cases as our answer
-		else return case1+case2;
+		else return case1 + case2;
 
 	}
 }
