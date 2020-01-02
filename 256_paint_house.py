13d3f25525838a4b5466e0a8bd90aaed5fafe101
Nikhil Agrawal <nikrasiya@gmail.com>
2020-01-02 16:45:39 -0500

    coin_change_complete

---
+++ 256_paint_house.py
@@ -0,0 +1,27 @@
+"""
+    There are a row of n houses, each house can be painted with one of the three colors: red, blue or green.
+    The cost of painting each house with a certain color is different.
+    You have to paint all the houses such that no two adjacent houses have the same color.
+
+    The cost of painting each house with a certain color is represented by a n x 3 cost matrix.
+    For example, costs[0][0] is the cost of painting house 0 with color red; costs[1][2]
+    is the cost of painting house 1 with color green, and so on.
+    Find the minimum cost to paint all houses.
+
+    Follow Up: Optimize for Space
+"""
+
+
+class Solution:
+    def minCost(self, costs):
+        pass
+
+
+if __name__ == '__main__':
+    """
+              R   B   G
+    """
+    costs = [[17, 2, 17],
+             [16, 16, 5],
+             [14, 3, 19]]
+    obj1 = Solution()
