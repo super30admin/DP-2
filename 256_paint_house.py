c0cd40f4b05aa4a439a671fc2d780ff1dea258d8
Nikhil Agrawal <nikrasiya@gmail.com>
2020-01-05 08:18:39 -0500

    all problems complete updated as per class

--- 256_paint_house.py
+++ 256_paint_house.py
@@ -46,63 +46,90 @@ class Solution:
             dp[house][2] = costs[house][2] + min(dp[house - 1][0], dp[house - 1][1])
         return min(dp[house][0], dp[house][1], dp[house][2])
 
-    def min_cost_brute_force(self, costs):
-        return self._helper(costs, 0, 0, [0, 1, 2])
+    # def min_cost_brute_force(self, costs):
+    #     return self._helper(costs, 0, 0, [0, 1, 2])
+    #
+    # def _helper(self, costs: int, min_cost: int, house_number: int, remaining_color: list):
+    #     # base case
+    #     if house_number == len(costs):
+    #         return min_cost
+    #
+    #     # case 1
+    #     # we choose to paint the house with red, then we can only choose between blue and green
+    #     if 0 in remaining_color:
+    #         red_cost = self._helper(costs, min_cost + costs[house_number][0], house_number + 1, [1, 2])
+    #     else:
+    #         red_cost = inf
+    #     # case 2
+    #     # we choose to paint the house with blue, then we can only choose between red and green
+    #     if 1 in remaining_color:
+    #         blue_cost = self._helper(costs, min_cost + costs[house_number][1], house_number + 1, [0, 2])
+    #     else:
+    #         blue_cost = inf
+    #     # case 3
+    #     # we choose to paint the house with green, then we can only choose between red and blue
+    #     if 2 in remaining_color:
+    #         green_cost = self._helper(costs, min_cost + costs[house_number][2], house_number + 1, [0, 1])
+    #     else:
+    #         green_cost = inf
+    #     return min(red_cost, blue_cost, green_cost)
 
-    def _helper(self, costs: int, min_cost: int, house_number: int, remaining_color: int):
+    def min_cost_brute_force(self, costs):
+        """
+            // Time Complexity : O(3 * 2^n)
+                        We will have three trees. One for each color
+                        'n' is the number of houses.
+            // Space Complexity : O(n) if stack space is considered
+        """
+        # initial amount is 0
+
+        # choosing red color for the first house
+        case_1 = self._helper(costs, 0, 0, 0)
+        # choosing blue color for the first house
+        case_2 = self._helper(costs, 0, 0, 1)
+        # choosing green color for the first house
+        case_3 = self._helper(costs, 0, 0, 2)
+
+        return min(case_1, case_2, case_3)
+
+    def _helper(self, costs: int, amount: int, house: int, color: int):
         # base case
-        if house_number == len(costs):
-            return min_cost
+        if house == len(costs):
+            return amount
+
+        # for each house we will have two cases
+        case_1 = case_2 = 0
+
+        # logic
 
         # case 1
-        # we choose to paint the house with red, then we can only choose between blue and green
-        if 0 in remaining_color:
-            red_cost = self._helper(costs, min_cost + costs[house_number][0], house_number + 1, [1, 2])
-        else:
-            red_cost = inf
-        # case 2
-        # we choose to paint the house with blue, then we can only choose between red and green
-        if 1 in remaining_color:
-            blue_cost = self._helper(costs, min_cost + costs[house_number][1], house_number + 1, [0, 2])
-        else:
-            blue_cost = inf
-        # case 3
-        # we choose to paint the house with green, then we can only choose between red and blue
-        if 2 in remaining_color:
-            green_cost = self._helper(costs, min_cost + costs[house_number][2], house_number + 1, [0, 1])
-        else:
-            green_cost = inf
-        return min(red_cost, blue_cost, green_cost)
+        # we choose to paint the current house with red color
+        if color == 0:
+            # add the cost to paint the current house with red
+            # we can choose blue for the next house
+            case_1 = self._helper(costs, amount + costs[house][0], house + 1, 1)
+            # or we can choose green for the next house
+            case_2 = self._helper(costs, amount + costs[house][0], house + 1, 2)
 
-    # def min_cost_brute_force(self, costs):
-    #     case_1 = self.helper(costs, costs[0][0], 1, 0)
-    #     case_2 = self.helper(costs, costs[0][1], 1, 1)
-    #     case_3 = self.helper(costs, costs[0][2], 1, 2)
-    #     return min(case_1, case_2, case_3)
-    #
-    # def helper(self, costs: int, total_cost: int, house_number: int, prev_chosen_color: int):
-    #     # base case
-    #     print(prev_chosen_color)
-    #     if prev_chosen_color == 0:
-    #         remaining_color = (1, 2)
-    #     elif prev_chosen_color == 1:
-    #         remaining_color = (0, 2)
-    #     else:
-    #         remaining_color = (0, 1)
-    #
-    #     if house_number >= len(costs) - 1:
-    #         return total_cost + min(costs[house_number][remaining_color[0]], costs[house_number][remaining_color[1]])
-    #
-    #     # logic
-    #     # case 1
-    #     # we choose to paint the house with 1st remaining color color
-    #     case_1 = self.helper(costs, total_cost + costs[house_number][remaining_color[0]], house_number + 1,
-    #                          remaining_color[0])
-    #     # case 2
-    #     # we choose to paint the house with 2nd color remaining color
-    #     case_2 = self.helper(costs, total_cost + costs[house_number][remaining_color[1]], house_number + 1,
-    #                          remaining_color[1])
-    #     return min(case_1, case_2)
+        # case 2
+        # we choose to paint the current house with blue color
+        elif color == 1:
+            # add the cost to paint the current house with blue
+            # we can choose red for the next house
+            case_1 = self._helper(costs, amount + costs[house][1], house + 1, 0)
+            # or we can chose green for the next house
+            case_2 = self._helper(costs, amount + costs[house][1], house + 1, 2)
+
+        # case 3
+        # we choose to paint the current house with green color
+        elif color == 2:
+            # add the cost to pain the current house with green
+            # we can choose red for the next house
+            case_2 = self._helper(costs, amount + costs[house][2], house + 1, 0)
+            # or we can choose blue for the next house
+            case_1 = self._helper(costs, amount + costs[house][2], house + 1, 1)
+
+        return min(case_1, case_2)
 
 
 if __name__ == '__main__':
@@ -114,9 +141,15 @@ if __name__ == '__main__':
              [7, 16, 5],
              [4, 6, 7],
              [1, 50, 100]]
-    # print(h.min_cost_brute_force(costs))
+    print(h.min_cost_brute_force(costs))
     print(h.min_cost(costs))
     costs = [[5, 17, 4],
              [6, 17, 3],
              [21, 1, 10]]
+    print(h.min_cost_brute_force(costs))
+    print(h.min_cost(costs))
+    costs = [[17, 2, 17],
+             [16, 16, 5],
+             [14, 3, 19]]
+    print(h.min_cost_brute_force(costs))
     print(h.min_cost(costs))
