ba1f17b8e672974694e668329a2953fc23a27fef
Gayathri Ganesh <gayathrig269@gmail.com>
2020-01-09 19:49:42 -0800

    Completed paint house problem - DP1

--- BigNProblem_21.cpp
+++ BigNProblem_21.cpp
@@ -1,12 +1,22 @@
-int calcpainthouses(int p[][4])
+// Time Complexity :O(N)
+// Space Complexity :O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+//Could not check all Leetcode conditions
+
+
+// Your code here along with comments explaining your approach
+//CODE IN C++//
+
+int calcpainthouses(int p[][3]) //function takes array as argument
 {
-    int m=p[0][0];
-    int min_cost=0;
+    int m=p[0][0]; //initializing iterator as m
+    int min_cost=0; //declaring cost as 0
     int r = sizeof(p[0])/sizeof(p[0][0]);
-    for(int i=0;i<r;i++)
+    for(int i=0;i<r;i++) //run the loop for N = size of column iterations
     {      
-                m=min(min(p[i][0],p[i][1]),p[i][2]);
-                 min_cost+=m;
+                m=min(min(p[i][0],p[i][1]),p[i][2]); //calculate the min of paint cost from every row
+                 min_cost+=m; //add iterator value to the min cost
     }
     return min_cost;
 }
