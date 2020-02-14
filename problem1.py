02e582f1c19eeda54ae541ef2bced0ceb94fec7a
Jay Vaghasiya <jayvaghasiya1@gmail.com>
2020-02-13 18:59:19 -0700

    Solved Paint House

--- problem1.py
+++ problem1.py
@@ -1,23 +1,27 @@
-#  Time Complexity : O(n)
-#  Space Complexity : O(n)
-#  Did this code successfully run on Leetcode : Yes( Ran it on LintCode)
-#  Any problem you faced while coding this : After looking through discussion found that It has simple splution and was easy to understand 
-
-
+# Time Complexity : O(n) (Where n is total houses)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Three line explanation of solution in plain english:
+# - This can be solved using dynamic programming. At every house chose one color and from previous house chose minimum from available other two color. This way in the last house we will have value of minimum for given color choise.
 
 class Solution:
-    """
-    @param costs: n x 3 cost matrix
-    @return: An integer, the minimum cost to paint all houses
-    """
-    def minCost(self, costs):
-        # write your code here
-        if len(costs) == 0:
+    def minCost(self, costs: List[List[int]]) -> int:
+#       Handle edge case
+        if not costs:
             return 0
         
-        for i in range(1, len(costs)):
+        ln = len(costs)
+        
+#       If only one house is given chose cheapest color.
+        if ln == 1:
+            return min(costs[0])
+        
+#       From second house run loop till the last house
+        for i in range(1, ln):
+#           For every color add it's cost and from previous house chose minimum cost from the two avilable color.
             costs[i][0] = costs[i][0] + min(costs[i-1][1], costs[i-1][2])
             costs[i][1] = costs[i][1] + min(costs[i-1][0], costs[i-1][2])
             costs[i][2] = costs[i][2] + min(costs[i-1][0], costs[i-1][1])
-        
-        return min(costs[-1][0], costs[-1][1], costs[-1][2])
+            
+#       The last house will have min value for chosing corresponding color.
+        return min(costs[ln-1])
