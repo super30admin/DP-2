c39808a8c0a9e1004759286ebef4047f3fc8affd
jainsupriya <supriya.jain@sjsu.edu>
2020-02-15 22:00:11 -0800

    Added Paint House solution

--- PaintHouse.java
+++ PaintHouse.java
@@ -1,57 +1,24 @@
-/*
-Recursive Approach
-// Time Complexity : 2^n
-// Space Complexity : O(m+n)
-// Did this code successfully run on Leetcode :Yes
+i// Time Complexity : O(n) where n is the number of houses
+// Space Complexity : O(1) as we are not using any extra space
+// Did this code successfully run on Leetcode : Yes
 // Your code here along with comments explaining your approach
-if we have robbed one house we can not rob adjancent house. at every house We would be making decision that whether to rob house or not. If we rob the house we would then we recursively exploring house next to the adjancent house(index+2) and if we dont choose we would  recursively explore from adjancent house
-*/
+/*
+1. In brute force manner At every house we have two choices. if first house is red then next house we can color either green or blue. if first house we colored blue next house we can color either green or red.. so on. So we will be observing all the paths
 
+Dynamic programming
+1. we start from 2nd house. if we choose to color this house as red then we should have colored 1st house as blue or green. So cost of coloring this house would be cost of coloring this house as red plus minimum price of coloring prev house as green or blue.
+*/
 class Solution {
-    public int rob(int[] nums) {
-        return robUtil(nums, 0, 0);
-    }
-    public int robUtil(int[]nums, int index, int max){
+    public int minCost(int[][] costs) {
         //base case
-        if(index >= nums.length)
-            return max;
-        //choose the house
-        int case1 = robUtil(nums, index+2 , max+nums[index]);
-        // dont choose the house
-        int case2 = robUtil(nums, index+1, max);
-        return Math.max(case1, case2);
+        if(costs == null || costs.length ==0)
+            return 0;
+        int row = costs.length;
+        for(int i=1; i<costs.length; i++){
+            costs[i][0]= costs[i][0]+ Math.min(costs[i-1][1],costs[i-1][2]);
+            costs[i][1]= costs[i][1]+ Math.min(costs[i-1][0],costs[i-1][2]);
+            costs[i][2]= costs[i][2]+ Math.min(costs[i-1][0],costs[i-1][1]);
+        }
+        return Math.min(costs[row-1][0], Math.min(costs[row-1][1],costs[row-1][2]));
     }
 }
-/*      [1,2,3,1,6] 
-[3,1,6]        [2,3,1,6]    
-[6] [1,6]     [3,1,6] [1,6]
-
-Repeated sub problems
-*/
-
-
-/*
-DP Approach
-// Time Complexity : O(n)
-// Space Complexity : O(1)
-// Did this code successfully run on Leetcode :Yes
-// Your code here along with comments explaining your approach
-
-*/
-
-class Solution {
-    public int rob(int[] nums) {
-        return robUtil(nums, 0, 0);
-    }
-    public int robUtil(int[]nums, int index, int max){
-        //base case
-        if(index >= nums.length)
-            return max;
-        //choose the house
-        int case1 = robUtil(nums, index+2 , max+nums[index]);
-        // dont choose the house
-        int case2 = robUtil(nums, index+1, max);
-        return Math.max(case1, case2);
-    }
-}
-
