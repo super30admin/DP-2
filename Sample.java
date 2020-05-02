7b23ac5d7c0339403eeb368d805c26044c15f071
Raju Datla <rajudatla@Rajus-MacBook-Pro.local>
2020-05-02 15:12:08 -0700

    Completed DP-2

--- Sample.java
+++ Sample.java
@@ -63,3 +63,28 @@ class Solution {
 }
 
 //painting a house
+//Time o(n)
+//space: o(1) as i/p array size and created array size is same
+
+class Solution {
+    public int minCost(int[][] costs) {
+        if(costs == null || costs.length==0)
+            return 0;
+        int m = costs.length, n = costs[0].length;
+        
+        int[][] dp = new int[m][n];
+        
+        for(int i=0;i<n;i++)
+            dp[0][i] = costs[0][i];
+        
+        for(int i=1;i<m;i++){
+            //if I choose red for the previous house
+            dp[i][0] = costs[i][0] + Math.min(dp[i-1][1],dp[i-1][2]);
+            //if I choose blue for the previous house
+            dp[i][1] = costs[i][1] + Math.min(dp[i-1][0],dp[i-1][2]);
+            //if I choose green for the previous house
+            dp[i][2] = costs[i][2] + Math.min(dp[i-1][0],dp[i-1][1]);
+        }
+       return Math.min(dp[m-1][0],Math.min(dp[m-1][1],dp[m-1][2]));
+    }
+}
