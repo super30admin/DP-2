db0cee78064eac98fe4c8dbbb4798c09c4d9d84c
Sarthak Sugandhi <47070701+sarthaksugandhi9@users.noreply.github.com>
2020-06-04 20:08:35 -0700

    Create Problem_2_DP.java

---
+++ Problem_2_DP.java
@@ -0,0 +1,23 @@
+// Time complexity - O(nm)
+// Space complexity - O(1)
+
+// Implemented using DP.
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        for(int i = 0;i<dp.length;i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i< dp.length;i++){
+            for(int j = 1; j < dp[0].length; j++){
+                if(j < coins[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]];
+                }
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
