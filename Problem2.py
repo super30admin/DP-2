a81d09186976b61e930bc2b0f37f53bb8904daa0
Shantanu609 <shantanu_shinde_@mail.fresnostate.edu>
2020-06-07 10:12:15 -0700

    Completed DP-2

--- Problem2.py
+++ Problem2.py
@@ -5,43 +5,43 @@
 # Your code here along with comments explaining your approach
 
 class Solution:
-    # def change(self,amount, coins):
-    #     if amount == 0:
-    #         return 1 
-    #     if not coins or len(coins) == 0:
-    #         return 0 
-    #     dp = [[0 for _ in range(amount+1)] for _ in range(len(coins)+1)]
-    #     for i in range(len(dp)):
-    #         dp[i][0] = 1 
-    #     for i in range(1,len(dp)):
-    #         for j in range(1,len(dp[0])):
-    #             if j < coins[i-1]:
-    #                 dp[i][j] = dp[i-1][j]
-    #             else:
-    #                 dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
-    #     return dp[-1][-1]
-    
     def change(self,amount, coins):
-        if amount == 0 :
+        if amount == 0:
             return 1 
         if not coins or len(coins) == 0:
             return 0 
-        return self.helper(amount,coins,0,0)
+        dp = [[0 for _ in range(amount+1)] for _ in range(len(coins)+1)]
+        for i in range(len(dp)):
+            dp[i][0] = 1 
+        for i in range(1,len(dp)):
+            for j in range(1,len(dp[0])):
+                if j < coins[i-1]:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
+        return dp[-1][-1]
+    
+    # def change(self,amount, coins):
+    #     if amount == 0 :
+    #         return 1 
+    #     if not coins or len(coins) == 0:
+    #         return 0 
+    #     return self.helper(amount,coins,0,0)
         
     
-    def helper(self,amount,coins,index,ways):
-        # base 
-        if amount == 0:
-            return 1
-        if amount < 0 or index == len(coins):
-            return 0 
-        # logic 
-        # choosen 
-        case1 = self.helper(amount - coins[index],coins,index,ways)
+    # def helper(self,amount,coins,index,ways):
+    #     # base 
+    #     if amount == 0:
+    #         return 1
+    #     if amount < 0 or index == len(coins):
+    #         return 0 
+    #     # logic 
+    #     # choosen 
+    #     case1 = self.helper(amount - coins[index],coins,index,ways)
         
-        #not choosen
-        case2 = self.helper(amount,coins,index+1,ways)
-        return case1 + case2 
+    #     #not choosen
+    #     case2 = self.helper(amount,coins,index+1,ways)
+    #     return case1 + case2 
 
 if __name__ == "__main__":
     s = Solution()
