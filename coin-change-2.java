b27e941f5a2fb47074d55795ec4c32361d0be0f8
Darshan <sanghavi.da@husky.neu.edu>
2020-06-08 21:44:49 -0400

    Update coin-change-2.java

--- coin-change-2.java
+++ coin-change-2.java
@@ -1,6 +1,30 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n*m) n = coins, m = amount
+// Space Complexity : O(n) n = coins
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this : No
 
 
+class Solution {
+    public int change(int amount, int[] coins) {
+        if(coins.length<1 || coins == null){
+            return -1;
+        }
+        int dp[][] = new int [coins.length + 1][amount + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (j < coins[i - 1]) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = dp[i - 1][j]+ dp[i][j - coins[i - 1]];
+            }
+        }
+       int result = dp[coins.length][amount]; 
+        return result;
+    }
+}
