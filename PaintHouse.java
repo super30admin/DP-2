eeca2fa2cc49d160f654b33a01e17134e2ab29b4
Arpit Sharma <arpit317@gmail.com>
2020-07-17 13:53:21 -0700

    Update PaintHouse.java

--- PaintHouse.java
+++ PaintHouse.java
@@ -1,3 +1,5 @@
+//Brute Force
+
 class Solution {
     public int minCost(int[][] costs) {
         int case0 = helper(costs, 0, 0, 0); //red
@@ -31,5 +33,25 @@ class Solution {
         return Math.min(case1, Math.min(case2, case3));
         
         
+    }
+}
+
+
+// DP
+
+class Solution {
+    public int minCost(int[][] costs) {
+        if(costs == null || costs.length == 0) return 0;
+        int[][] dp = new int[costs.length][3];
+        for(int j = 0; j < 3; j++){
+            dp[0][j] = costs[0][j];
+        }
+        
+        for(int i = 1; i < costs.length; i++){
+            dp[i][0] = costs[i][0] + Math.min(dp[i-1][1],dp[i-1][2]);
+            dp[i][1] = costs[i][1] + Math.min(dp[i-1][0],dp[i-1][2]);
+            dp[i][2] = costs[i][2] + Math.min(dp[i-1][0],dp[i-1][1]);
+        }
+        return Math.min(dp[costs.length-1][0], Math.min(dp[costs.length-1][1],dp[costs.length-1][2]));
     }
 }
