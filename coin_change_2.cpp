54636d88487042cd5be560ca3ed1907b8c688a02
asiddiqua004 <48198420+asiddiqua004@users.noreply.github.com>
2020-09-11 13:58:53 -0700

    Update coin_change_2.cpp

--- coin_change_2.cpp
+++ coin_change_2.cpp
@@ -1,3 +1,71 @@
+// Time Complexity : O(2^mn) m:is the amount, n is the number of coins
+//Space  Complexity : O(2^mn)
+class Solution {
+public:
+    int ways=0;
+    int change(int amount, vector<int>& coins) {
+        //edge
+         if(amount==0)
+            return 1;
+        if(coins.size()==0)
+            return 0;
+        
+        //recurse
+        return recurse(coins, 0, 0, amount);
+    }
+    
+    
+    int recurse(vector<int>& coins, int index, int curr_amount, int amount, ){
+        //edge
+        if(curr_amount == amount){
+            return 1;
+        }else if(curr_amount> amount || index >= coins.size()){
+            return 0;
+        }
+        //choose + //dont_choose
+        int curr_ways = recurse(coins, index, curr_amount+coins[index], amount) + recurse(coins, index+1, curr_amount, amount);
+        return curr_ways;
+    }
+};
+
+//Memoization solution
+// Time Complexity : O(2^mn) m:is the amount, n is the number of coins
+//Space  Complexity : O(2^mn)
+class Solution {
+public:
+    int ways=0;
+    int change(int amount, vector<int>& coins) {
+        //edge
+         if(amount==0)
+            return 1;
+        if(coins.size()==0)
+            return 0;
+        
+        //recurse
+        vector<vector<int>> memo(coins.size()+1, vector<int>(amount+1,0));
+        memo[0][0]=1;
+        return recurse(coins, 0, 0, amount, memo);
+    }
+    
+    
+    int recurse(vector<int>& coins, int index, int curr_amount, int amount, vector<vector<int>> &memo){
+        //edge
+        if(curr_amount == amount){
+            return 1;
+        }else if(curr_amount>amount || index >= coins.size()){
+            return 0;
+        }
+        
+        if(memo[index+1][curr_amount+1] !=0){
+            return memo[index+1][curr_amount+1];
+        }
+        //choose + //dont_choose
+        int curr_ways = recurse(coins, index, curr_amount+coins[index], amount, memo) + recurse(coins, index+1, curr_amount, amount,memo);
+        memo[index+1][curr_amount+1] = curr_ways;
+        return curr_ways;
+    }
+};
+
 // Time Complexity : O(mn) m:is the amount, n is the number of coins
 
 // Space Complexity : O(n)
@@ -48,7 +116,7 @@ public:
             return 1;
         }
         //1.B
-        if(  coins.size()==0 ){
+        if(coins.size()==0 ){
             return 0;
         }
         //1.C
@@ -60,3 +128,40 @@ public:
     }
 };
 
+
+// Time Complexity : O(mn) m:is the amount, n is the number of coins
+// Space Complexity : O(n)
+//Simple recursive solution
+
+class Solution {
+public:
+    int ways=0;
+    int change(int amount, vector<int>& coins) {
+        //edge
+         if(amount==0)
+            return 1;
+        if(coins.size()==0)
+            return 0;
+        
+        //recurse
+        vector<vector<int>> memo(coins.size()+1, vector<int>(amount+1,0));
+        memo[0][0]=1;
+        int rows = coins.size()+1;
+        int cols = amount+1;
+        
+        for(int i=1; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(coins[i-1]>j){
+                    memo[i][j] = memo[i-1][j];
+                }else{
+                    memo[i][j] = memo[i-1][j] + memo[i][j-coins[i-1]];
+                }
+                //cout<<memo[i][j]<<"\t";
+            }
+           // cout<<"\n";
+        }
+        
+        return memo[rows-1][cols-1];
+    }
+    
+};
