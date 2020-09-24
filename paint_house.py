14f909eb8a4b572a00c0393eac97f9957108cf31
Piyush Kurkure <piyushk10@gmail.com>
2020-09-24 14:53:20 -0700

    Tried DP-2

---
+++ paint_house.py
@@ -0,0 +1,9 @@
+# Time Complexity : -
+# Space Complexity : -
+
+# Did this code successfully run on Leetcode : -
+# Any problem you faced while coding this : Not able to solve it
+
+# class Solution:
+#     def minCost(self, costs: List[List[int]]) -> int:
+        
