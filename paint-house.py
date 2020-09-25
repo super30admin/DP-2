6a481675fafb7ae0398422ff24630c19b636c9cb
Karan Chandra <43771528+Karan1914@users.noreply.github.com>
2020-09-24 20:54:19 -0500

    Add files via upload

---
+++ paint-house.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        n = len(costs)
+        if(n==0) : 
+            return 0
+        
+        for i in range(1,n):
+            costs[i][0] += min(costs[i-1][1],costs[i-1][2])
+            costs[i][1] += min(costs[i-1][0],costs[i-1][2])            
+            costs[i][2] += min(costs[i-1][1],costs[i-1][0])            
+        
+        return min(costs[-1])
+    
+    # Time Compexity : O(n)
