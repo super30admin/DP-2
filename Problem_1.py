e900422927c5fa03f301ed6f2fd2e44cb365395f
Kanika <kanika.khanna@sjsu.edu>
2020-10-24 16:56:50 -0700

    Paint house with DP

--- Problem_1.py
+++ Problem_1.py
@@ -1,15 +1,56 @@
 """
+Time Complexity : O(m) whene m is numbr of houses
+Space Complexity : O(1) 
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : No
+
+
+Your code here along with comments explaining your approach:
+This is a DP solution in case we can not manipulate the initial array. At every row, we have memoised
+what can be the minimum value for red , blue and green from the previous value and hence can be used
+to find minimum cost after all the rows have been iterated
+"""
+
+
+class Solution:
+    def minCost(self, costs):
+        # write your code here
+        if not costs or costs == None:
+            return 0
+        m = len(costs)
+        n = len(costs[0])
+        cr, cb, cg = 0, 0, 0
+        for i in range(0, m):
+            cr, cb, cg = costs[i][0]+min(cb, cg), costs[i][1] + \
+                min(cr, cg), costs[i][2]+min(cb, cr)
+        return(min(cr, cb, cg))
+
+
+"""
+This is a DP solution in case we can manipulate the initial array
+"""
+
+
+class Solution:
+    def minCost(self, costs):
+        # write your code here
+        if not costs or costs == None:
+            return 0
+        m = len(costs)
+        n = len(costs[0])
+        for i in range(1, m):
+            for j in range(n):
+                costs[i][j] += min(costs[i-1][:j]+costs[i-1][j+1:])
+        return(min(costs[-1]))
+
+
+"""
 This is the recursive code as of now which I wrote, will be modifying it to DP and submitting again
 once I understand how DP can be handled here. Right now, it gives time limit exceeded. Thanks
 """
 
 
 class Solution:
-    """
-    @param costs: n x 3 cost matrix
-    @return: An integer, the minimum cost to paint all houses
-    """
-
     def minCost(self, costs):
         # write your code here
         if not costs or costs == None:
