abc3ef3898d0ac3613137532f0cdda601143c8f2
bhargavajonnalagadda <74167176+bhargavajonnalagadda@users.noreply.github.com>
2020-12-20 17:32:44 -0500

    Time: 96 ms (69.23%), Space: 24.4 MB (86.67%) - LeetHub

---
+++ coin-change-2/coin-change-2.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int Change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] =1;
+        
+        foreach(int coin in coins)
+        {
+            for(int x=coin;x<amount+1;++x)
+            {
+                dp[x] += dp[x-coin];
+            }
+        }
+        
+        return dp[amount];
+        
+    }
+}
