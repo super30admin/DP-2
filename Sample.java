c53f59b69e1bb7c531a720968fc0e93f405fb666
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-21 13:56:02 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,18 @@
 
 
 // Your code here along with comments explaining your approach
+  public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
+
+    for (int coin : coins) {
+      for (int x = coin; x < amount + 1; ++x) {
+        dp[x] += dp[x - coin];
+      }
+    }
+    return dp[amount];
+  }
+}
+
+// time complexity : O (n X amount)
+// space complexity : O(amount)
