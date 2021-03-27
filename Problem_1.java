1bc906b792767e2b557a52255778fdfb5cbb3821
Akhil Varupula <akhilvarupula@Akhils-MacBook.attlocal.net>
2021-03-27 14:24:54 -0700

    completed DP-2

---
+++ Problem_1.java
@@ -0,0 +1,20 @@
+// Time Complexity : O(n) n is the rows of houses
+// Space Complexity:)O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : yes
+// Your code here along with comments explaining your approach
+//DP
+
+class Solution {
+    public int minCost(int[][] costs) {
+        if(costs==null || costs.length==0 )return 0;
+        for(int i=costs.length-2;i>=0;i--){
+            costs[i][0]=costs[i][0]+Math.min(costs[i+1][1], costs[i+1][2]);
+            costs[i][1]=costs[i][1]+Math.min(costs[i+1][0], costs[i+1][2]);
+            costs[i][2]=costs[i][2]+Math.min(costs[i+1][0], costs[i+1][1]);
+        }
+        return Math.min(costs[0][0], Math.min(costs[0][1],costs[0][2]));
+    }
+}
+
+
