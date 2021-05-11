fb6a568e9eeab23071109fec53e7fad113841fec
sp9491 <42806187+sp9491@users.noreply.github.com>
2021-05-11 01:50:47 -0400

    Create CoinChange2.java

---
+++ CoinChange2.java
@@ -0,0 +1,15 @@
+//Time complexiyt - O(n * amt)
+//Space  - O(amt)
+class Solution {
+  public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
+
+    for (int coin : coins) {
+      for (int x = coin; x < amount + 1; ++x) {
+        dp[x] += dp[x - coin];
+      }
+    }
+    return dp[amount];
+  }
+}
