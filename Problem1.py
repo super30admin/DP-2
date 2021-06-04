38a72d7a4e8a21f71bbb01371429fe002481353d
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-06-04 20:38:10 +0530

    Problem 2 Recursion done

--- Problem1.py
+++ Problem1.py
@@ -1,4 +1,17 @@
-# Time Complexity : 
-# Space Complexity : 
-# Did this code successfully run on Leetcode : 
-# Any problem you faced while coding this : 
+# Time Complexity : O(3**n)
+# Space Complexity : O(logn)
+# Did this code successfully run on Leetcode : No   
+# Any problem you faced while coding this : Couldn't solve with DP
+
+
+def minAmount(arr):
+    return helper(arr, 0, N)
+
+
+def helper(arr, ind, prevColour,amount):
+    if ind > arr.length:
+        return amount
+    else:
+        if ind  == 0:
+            return min(helper(arr,1,0,amount + arr[ind][0]),helper(arr,1,1,arr[ind][1]),helper(arr,1,2,arr[ind][2]))
+        return min(helper(arr,1,0,amount + arr[ind][0]),helper(arr,1,1,arr[ind][1])) if prevColour == 2 else min(helper(arr,1,0,amount + arr[ind][0]),helper(arr,1,2,arr[ind][2])) if prevColour == 1 else min(helper(arr,1,1,amount + arr[ind][1]),helper(arr,1,2,arr[ind][2]))
