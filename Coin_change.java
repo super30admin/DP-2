f08ffe8cb8c24215a2c516ac4922281b4e59f682
chaitanya003 <chaitanyakhinvasara33@gmail.com>
2021-06-07 00:14:45 +0530

    Completed DP-2

---
+++ Coin_change.java
@@ -0,0 +1,29 @@
+//Time Complexity :  O(m*n)
+// Space Complexity : O(m*n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        if(amount == 0)
+            return 1;
+
+        int[][] dp = new int[coins.length + 1][amount+1];
+        for(int i=0; i< dp.length; i++) {
+            dp[i][0] = 1;
+        }
+
+        for(int i=1; i< dp.length; i++) {
+            for(int j=1; j<dp[0].length; j++) {
+                if(j < coins[i-1]) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j]  = dp[i-1][j] + dp[i][j-coins[i-1]];
+                }
+            }
+        }
+
+        return dp[dp.length-1][dp[0].length-1];
+    }
+} 
