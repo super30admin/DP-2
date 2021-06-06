326c600f559d1f947ee6cb7469f1a350d7abe859
Puri <vxp190034@utdallas.edu>
2021-06-05 20:01:11 -0500

    DP-2 Sprint Completed

---
+++ PaintHouse.java
@@ -0,0 +1,20 @@
+/**
+ * @author Vishal Puri
+ * // Time Complexity : O(n)
+ * // Space Complexity : O(1)
+ * // Did this code successfully run on Leetcode : Yes
+ * // Any problem you faced while coding this :
+ */
+
+public class PaintHouse {
+    public int getMinimumPrice(int[][] cost) {
+        int price = 0;
+        for (int i = 1; i < cost.length; i++) {
+            cost[i][0] += Math.min(cost[i - 1][1], cost[i - 1][2]);
+            cost[i][1] += Math.min(cost[i - 1][0], cost[i - 1][2]);
+            cost[i][2] += Math.min(cost[i - 1][1], cost[i - 1][0]);
+        }
+        price = Math.min(Math.min(cost[cost.length - 1][0], cost[cost.length - 1][1]), cost[cost.length - 1][2]);
+        return price;
+    }
+}
