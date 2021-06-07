a3986422dda3beadb41cab388d3cee4a4a1287dc
palakbaid95 <56458390+palakbaid95@users.noreply.github.com>
2021-06-07 11:11:31 -0700

    Completed Dynamic Programming 2

---
+++ question23_coinChange2.java
@@ -0,0 +1,47 @@
+package DynamicProgramming2;
+
+public class question23_coinChange2 {
+
+    /*
+    Here we are bothered if the path is able to make the amount = 0.
+    Therefore, we are checking the total number of valid paths which make the amount = 0.
+    Count every valid path and Do Not count non valid paths.
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    Since the time limit exceed by the above approch. Now we will be doing the Dynamic Programming.
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+
+    Since we have to return value. The dp array is the auxillary data structure. So space will not be considered.
+
+    */
+/*
+    Dynamic Programming approach :
+*/
+
+    public int change(int amount, int[] coins) {
+        if(coins == null || coins.length == 0) return 0;
+
+        int[][] dp = new int[coins.length + 1][amount + 1];
+
+        for(int i = 0 ; i < dp.length ; i++) {
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1 ; i < dp.length ; i++) {
+            for(int j = 1 ; j < dp[0].length ; j++) {
+                if(j < coins[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+
+    public static void main(String[] args) {
+    }
+}
