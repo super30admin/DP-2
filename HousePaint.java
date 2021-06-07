d155c4604e01165b83835b63f508f4c1842de61a
Dhanashree Kamath <dhanashreekamath92@gmail.com>
2021-06-07 15:58:52 -0700

    house paint using recursion

---
+++ HousePaint.java
@@ -0,0 +1,44 @@
+// Time Complexity : O(3*2^n) = O(2^n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public int minCost(int[][] costs) {
+        //using recursion
+        //null check
+        if(costs == null || costs.length == 0) {
+            return 0;
+        }
+        
+        int case1 = helper(costs, 0, 0, 0); //blue
+        int case2 = helper(costs, 1, 0, 0); //green
+        int case3 = helper(costs, 2, 0, 0); //red
+        
+        return Math.min(case1, Math.min(case2, case3));
+        
+    }
+    
+    private int helper(int[][] costs, int color, int row, int minCost) {
+        //base
+        if(row == costs.length) return minCost;
+        
+        //logic
+        //choose 
+        if(color == 0) {//blue
+            return Math.min(helper(costs, 1, row+1, minCost+costs[row][0]), 
+                         helper(costs, 2, row+1, minCost+costs[row][0]));
+        }
+        if(color == 1) {//green
+            return Math.min(helper(costs, 0,row+1, minCost+costs[row][1]), 
+                         helper(costs, 2,row+1, minCost+costs[row][1]));
+        }
+         if(color == 2) { //red
+            return Math.min(helper(costs, 0,row+1, minCost+costs[row][2]), 
+                         helper(costs, 1,row+1, minCost+costs[row][2]));
+         }
+        
+         return 123;
+    }
+   
+}
