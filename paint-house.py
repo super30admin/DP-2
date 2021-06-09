ecd30fd36fb0d4da8c0ea009621cf4a4df91db49
Suman Bharti <suman.bharti30@gmail.com>
2021-06-09 00:37:42 -0400

    solved

---
+++ paint-house.py
@@ -0,0 +1,21 @@
+import copy
+
+
+class Solution:
+    def minCost(self, costs) -> int:
+
+        if len(costs) == 0: return 0
+
+        previous_row = costs[-1]
+        for n in reversed(range(len(costs) - 1)):
+            current_row = copy.deepcopy(costs[n])
+            # Total cost of painting nth house red?
+            current_row[0] += min(previous_row[1], previous_row[2])
+            # Total cost of painting nth house green?
+            current_row[1] += min(previous_row[0], previous_row[2])
+            # Total cost of painting nth house blue?
+            current_row[2] += min(previous_row[0], previous_row[1])
+            previous_row = current_row
+
+        return min(previous_row)
+
