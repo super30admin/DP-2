65a25dbdc5b57965048c83424266d65d7f0bfcb8
AdithyaHolla <56132230+AdithyaHolla@users.noreply.github.com>
2021-06-08 20:50:48 -0700

    Create Problem_1 - 2.py

---
+++ Problem_1 - 2.py
@@ -0,0 +1,17 @@
+"""
+
+Time Complexity : O(n) where n is number of houses.
+Space Complexity : O(1)
+    
+"""
+
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        if not costs or len(costs) == 0: return 0 
+        
+        for i in range(len(costs)-2, -1, -1):
+            costs[i][0] = costs[i][0] + min(costs[i+1][1], costs[i+1][2])
+            costs[i][1] = costs[i][1] + min(costs[i+1][0], costs[i+1][2])
+            costs[i][2] = costs[i][2] + min(costs[i+1][0], costs[i+1][1])
+        
+        return min(costs[0][0], costs[0][1], costs[0][2])
