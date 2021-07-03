6c1c5db4f8328dea2b5616581df1a9c236015624
Ramya Kothapalli <sairamyakothapalli@gmail.com>
2021-07-02 17:20:57 -0700

    Completed Dp-2

---
+++ CoinChange2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        if(coins == null || coins.length ==0)
+            return 0;
+        int dp[][] = new int[coins.length+1][amount+1];
+        
+        for(int i=1;i<coins.length+1;i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i=1;i<coins.length+1;i++){
+            for(int j=1;j<amount+1;j++){
+                //if()
+                if(j<coins[i-1])
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j]+dp[i][j-coins[i-1]];
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
