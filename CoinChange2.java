d471b17d4e70aed97b88e3a1c93d7263a5678bae
vg4838 <vg4838@g.rit.edu>
2021-08-28 05:02:18 -0400

    DP 2 - VIVEK (review)

---
+++ CoinChange2.java
@@ -0,0 +1,17 @@
+class CoinChange2 {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        dp[0][0] = 1;
+        for(int i=1;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                if(j < coins[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+}
