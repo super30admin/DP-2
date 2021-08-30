40a09845d369c5830cd05e7e94727c2d8e58074c
Sai Krishna Wuyyuru <54835344+w-s-k387@users.noreply.github.com>
2021-08-29 23:34:53 -0500

    updated  DP-2(painthouse)

--- PaintHouse.java
+++ PaintHouse.java
@@ -1,5 +1,16 @@
 package DP;
 
 public class PaintHouse {
+	 public int minCost(int[][] costs) {
+	        if(costs==null || costs.length==0) return 0;
+	        
+	        
+	        for(int i=costs.length-2;i>=0;i--){
+	            costs[i][0]=costs[i][0]+Math.min(costs[i+1][1],costs[i+1][2]);
+	            costs[i][1]=costs[i][1]+Math.min(costs[i+1][0],costs[i+1][2]);
+	            costs[i][2]=costs[i][2]+Math.min(costs[i+1][0],costs[i+1][1]);
+	        }
+	        return Math.min(costs[0][0],Math.min(costs[0][1],costs[0][2]));
+	    }
 
 }
