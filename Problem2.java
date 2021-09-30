d62963d292f60c1bb3d0915d83285c66d563da0b
Infernus <Infernus101@users.noreply.github.com>
2021-09-30 16:06:35 -0400

    DP-2 Completed - Please review

---
+++ Problem2.java
@@ -0,0 +1,25 @@
+// Time Complexity : O(N * S)
+// Space Complexity : O(S)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+import java.util.Arrays;
+
+public class Problem2 {
+
+    public static int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int i : coins)
+            for (int j = i; j <= amount; ++j)
+                dp[j] += dp[j-i];
+
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(change(5, new int[]{1, 2, 5}));
+    }
+
+}
