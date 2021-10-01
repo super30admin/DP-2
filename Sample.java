2d6856d9a4190f5f26e851f5dad2a11a64673d3d
Praveen Kumar Reddy Hande <praveenkr7191@gmail.com>
2021-09-30 22:21:13 -0700

    Add files via upload

--- Sample.java
+++ Sample.java
@@ -1,7 +1,20 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : primium problem unable to work in leetcode
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+class solution{
+    public minCost( int[][] costs){
+if (costs==null || costs.length==0){
+    return 0;
+}
+        for (int i=1;i<costs.length;i++){
+            costs[i][0]+= Math.min(costs[i-1][1],costs[i-1][2];
+            costs[i][1]+= Math.min(costs[i-1][0],costs[i-1][2];
+            costs[i][2]+= Math.min(costs[i-1][0],costs[i-1][1];
+        }
+        return Math.min(Math.min(costs[costs.length-1][0]),costs[costs.length-1][1]),costs[costs.length-1][2]))
+    }
+}
