77102475588b41292c95dea357dd00f2ce3c7c4e
Infernus <Infernus101@users.noreply.github.com>
2021-10-02 01:19:33 -0400

    tc sc

--- Problem1.java
+++ Problem1.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 public class Problem1 {
 
     public static int minCost(int[][] costs) {
