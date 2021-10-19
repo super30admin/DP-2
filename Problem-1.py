69ecd1546ec3dd23fb74d8a9d9b23cf2f8f3a660
Adhish Shrivastava <adhish70@gmail.com>
2021-10-19 16:44:47 -0400

    Initial commit

---
+++ Problem-1.py
@@ -0,0 +1,9 @@
+# 256. Paint House
+# https://leetcode.com/problems/paint-house/
+
+# Time complexiety: 
+# Space complexiety: 
+
+class Solution:
+    def minCost(self, costs) -> int:
+        pass
---
+++ Problem-2.py
@@ -0,0 +1,9 @@
+# 518. Coin Change 2
+# https://leetcode.com/problems/coin-change-2/
+
+# Time complexiety: 
+# Space complexiety: 
+
+class Solution:
+    def change(self, amount, coins) -> int:
+        pass
