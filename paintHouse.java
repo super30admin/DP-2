69c468fc526fee811c27f63de99e8af8b882a1a4
Swathi Nagaraja <44962250+SwathiAvadhani@users.noreply.github.com>
2021-10-25 18:21:16 -0500

    Completed DP2 - Please Review

---
+++ paintHouse.java
@@ -0,0 +1,30 @@
+// Time Complexity : O(2^n * 3)
+// Space Complexity : 
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+import java.util.*;
+
+class paintHouseSolution {
+    public static int minCost(int[][] costs) {
+        int min = Math.min(costs[0][0], Math.min(costs[0][1], costs[0][2]));
+
+        for (int i = 1; i < costs.length; i++) {
+            costs[i][0] = costs[i][0] + Math.min(costs[i-1][1], costs[i-1][2]);
+            costs[i][1] = costs[i][1] + Math.min(costs[i-1][0], costs[i-1][2]);
+            costs[i][2] = costs[i][2] + Math.min(costs[i-1][0], costs[i-1][1]);
+
+            min = Math.min(costs[i][0], Math.min(costs[i][1], costs[i][2]));
+
+        }
+        return min;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] costs = new int[][] {{17,2,17}, {16,16,5}, {14,3,9}};
+        int result;
+        result = minCost(costs);
+        System.out.println(result);
+    }
+}
