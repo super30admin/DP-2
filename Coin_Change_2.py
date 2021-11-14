18a29f39c51cbf9b7f9fb36c55b3b79c6786dd8b
tiwariarjun91 <tiwariarjun91@gmail.com>
2021-11-14 16:48:11 +0530

    Solved Coin Change

--- Coin_Change_2.py
+++ Coin_Change_2.py
@@ -1,15 +1,29 @@
 """
- Time Complexity :
- Space Complexity :
- Did this code successfully run on Leetcode :
- Any problem you faced while coding this :
+ Time Complexity : O(n)
+ Space Complexity : O(amount * coins)
+ Did this code successfully run on Leetcode : yes
+ Any problem you faced while coding this : no
 
 
  Your code here along with comments explaining your approach """
 def CoinChange2(amount, coins)-> int:
-    return helper(amount, coins, 0)
+    dp = [[0 for _ in range(amount + 1)] for _ in range(len(coins)+ 1)]
 
-def helper(amount, coins, combinations) -> int:
+    for i in range(0,len(dp) ):
+        dp[i][0] = 1
+
+    for i in range(1, len(dp)):
+        for j in range(1, len(dp[0])):
+            if(coins[i-1]> j):
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
+
+    return dp[len(dp)-1][len(dp[0])-1]
+
+output = CoinChange2(5, [1,2,5])
+
+"""def helper(amount, coins, combinations) -> int:
     # base
     if amount == 0:
         return 1
@@ -26,11 +40,11 @@ def helper(amount, coins, combinations) -> int:
 
     return case1 + case2
 
-output = CoinChange2(5, [1,2,5])
-print(output)
+
 
 output = CoinChange2(5, [2])
 print(output)
 
 output = CoinChange2(500, [200,40,100,5,1,600])
 print(output)
+"""
