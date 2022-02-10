b0c1a99397b29ad3ecbfaafcb6d955b4dee7d79f
amit singh <amitsingh5317@gmail.com>
2022-02-10 14:14:16 -0600

    amitsingh5317 init dp2

---
+++ _256_PaintHouse.java
@@ -0,0 +1,11 @@
+// space complexity - o(n)
+    	//time - o(n)
+      //Ran on leeetcode successfully : Yes
+      // Problem faced  : No
+    	//Approach : linear memuization
+
+class Solution {
+    public int minCost(int[][] costs) {
+
+    }
+}
