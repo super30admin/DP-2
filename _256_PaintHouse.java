b284cba2b36f5228bfdfff0406746483e91b4b2e
amit singh <amitsingh5317@gmail.com>
2022-02-10 18:59:22 -0600

    bruteforce backtracking for house paint

--- _256_PaintHouse.java
+++ _256_PaintHouse.java
@@ -2,10 +2,29 @@
     	//time - o(n)
       //Ran on leeetcode successfully : Yes
       // Problem faced  : No
-    	//Approach : linear memuization
+    	//Approach : brute force using back tracking 3^n
 
-class Solution {
-    public int minCost(int[][] costs) {
+      class Solution {
 
-    }
-}
+          int minCost = Integer.MAX_VALUE;
+
+          public int minCost(int[][] costs) {
+              minCost(costs, 0,-1, 0);
+              return minCost;
+          }
+
+          public void minCost(int[][] costs, int house, int color, int cost) {
+              if(house ==  costs.length){
+                  minCost  = Math.min(minCost, cost);
+                  return;
+              }
+
+              for(int j=0 ; j< costs[0].length; j++){
+                  if(j != color){
+                      minCost(costs, house+1, j ,  cost + costs[house][j]);
+                  }
+              }
+
+
+          }
+      }
