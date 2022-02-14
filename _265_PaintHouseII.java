094a6a3c0a5ce588a4866da52fc54f13cb2d287d
amit singh <amitsingh5317@gmail.com>
2022-02-14 04:39:54 -0600

    adding paint house 2 with constant space

--- _265_PaintHouseII.java
+++ _265_PaintHouseII.java
@@ -55,4 +55,64 @@ class Solution {
         }
         return minRow1;
     }
+
+    //Same as above but with constant space , no need for dp array
+    // space complexity - o(1)
+        //time - o(n*k)
+        //Ran on leeetcode successfully : Yes
+        // Problem faced  : No
+        //Approach : take a 2d dp array, the value of cell will be equal to sum of cost of painting by the current cell and
+        //minimum the cost of painting the house from previous row of different color.
+        //Now since there are k houses, maintain two variable for mincost1 and mincost2 for the previous row.
+
+    public int minCostII(int[][] costs) {
+
+
+
+        int minRow1 = Integer.MAX_VALUE;
+        int minColorRow1 = -1;
+        int minRow2 = Integer.MAX_VALUE;
+        int minColorRow2 = -1;
+
+        for(int i=0; i<costs.length; i++){
+
+            int min1 = Integer.MAX_VALUE;
+            int minColor1 = -1;
+            int min2 = Integer.MAX_VALUE;
+            int minColor2 = -1;
+
+            for(int j=0; j<costs[0].length; j++){
+
+                int temp = -1;
+
+                if(i==0){
+                    temp = costs[i][j];
+                }
+                else{
+                 if(j != minColorRow1) {
+                     temp = minRow1 + costs[i][j];
+                 }
+                 else{
+                    temp = minRow2 + costs[i][j];
+                 }
+
+                }
+                if(min1 >= temp){
+                    min2 = min1;
+                    minColor2 = minColor1;
+                    min1 = temp;
+                    minColor1 = j;
+                }
+                else if(min2 >= temp){
+                    min2 = temp;
+                    minColor2 = j;
+                }
+            }
+            minRow1 = min1;
+            minColorRow1 = minColor1;
+            minRow2 = min2;
+            minColorRow2 = minColor2;
+        }
+        return minRow1;
+    }
 }
