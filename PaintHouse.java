a8e84aaf415027d75496d6509186f76311a88116
Yashwant Bezawada <yashwant_b@me.com>
2022-03-12 23:32:12 -0600

    Update PaintHouse.java

--- PaintHouse.java
+++ PaintHouse.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(mn)
+// Time Complexity : O(n)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
