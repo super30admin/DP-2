ce2c5960b21780b89e91667d5c54240ebf01be73
VibhaDhar <dharvibha@gmail.com>
2022-04-02 14:04:52 -0500

    update

--- HouseColor.java
+++ HouseColor.java
@@ -1,6 +1,16 @@
 //TC: O(M*N)
 //SC: O(1)
 
+
+//This problem was asked at Amazon
+
+// Given a row of n houses, each house can be coloured with any of the three colours: let these be red blue or green. the cost for painting a particular house with a particular colour is going to be different. you have to colour the house in such a way that no two adjacent houses will have the same colour. the cost for painting  a house with any colour is represented by a n X 3 cost matrix where cost for painting house 0 with colour red is cost[0][0] and cost for painting house 4 with green colour is cost[4][2] and so on.....calculate the minimum cost to paint all houses. try to optimize it for space !! 
+
+
+
+
+
+
 class Solution
 {
     public int minCost(int [][] costs)
@@ -28,3 +38,9 @@ class Solution
         );
     }
 }
+
+// 
+
+
+
+
