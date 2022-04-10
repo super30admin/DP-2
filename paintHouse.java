cd82eb0ed220af25bedee10a16821e02539d8773
Laxmikant Bhaskar Pandhare <47066536+laxmikantbpandhare@users.noreply.github.com>
2022-04-10 11:19:11 -0700

    Update paintHouse.java

--- paintHouse.java
+++ paintHouse.java
@@ -17,7 +17,7 @@
 
     public class paintHouse{
     public static void main(String args[]) {
-                int costs[][]= new int[][] {{17,2,17},{16,16,5},{14,3,19}};
+                int costs[][]= new int[][] {{17, 2, 17},{16, 16, 5},{14, 3, 19}};
                 System.out.println(minCost(costs));
             }
             
@@ -29,11 +29,11 @@
         
         
             for(int i=1;i<costs.length;i++){
-                costs[i][0] += Math.min(costs[i-1][1],costs[i-1][2]);   
-                costs[i][1] += Math.min(costs[i-1][0],costs[i-1][2]);   
-                costs[i][2] += Math.min(costs[i-1][0],costs[i-1][1]);   
+                costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);   
+                costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);   
+                costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);   
             }
         
-            return Math.min(costs[costs.length-1][2],Math.min(costs[costs.length-1][0],costs[costs.length-1][1]));
+            return Math.min(costs[costs.length - 1][2],Math.min(costs[costs.length - 1][0],costs[costs.length - 1][1]));
         }
 }
