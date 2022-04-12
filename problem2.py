71f05199b99fb03f679a8a526155649f655f6778
Shrey Patel <shrey1124@gmail.com>
2022-04-12 01:59:51 -0500

    solutions added

--- problem2.py
+++ problem2.py
@@ -1,5 +1,6 @@
 '''https://leetcode.com/problems/coin-change-2/
-You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+You are given an integer array coins representing coins of different denominations 
+and an integer amount representing a total amount of money.
 
 Return the number of combinations that make up that amount. 
 If that amount of money cannot be made up by any combination of the coins, return 0.
