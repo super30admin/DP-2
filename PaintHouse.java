3a0e9913b012ca6d2aa3f34f0b770090052bceb1
arshmohd <irfan.arshad4@gmail.com>
2022-05-10 17:10:41 +0530

    Implemented the DP for paint-house basic variation of LC

---
+++ PaintHouse.java
@@ -0,0 +1,34 @@
+public class PaintHouse {
+
+  public static int minCost(int[][] costs) {
+    if (costs == null || costs.length == 0) {
+      return 0;
+    }
+
+    int[][] dp = new int[3][costs.length];
+
+    for (int idx = 0; idx < costs.length; idx++) {
+      if (idx == 0) {
+        dp[0][idx] = costs[idx][0];
+        dp[1][idx] = costs[idx][1];
+        dp[2][idx] = costs[idx][2];
+      } else {
+        dp[0][idx] = Math.min(dp[1][idx - 1], dp[2][idx - 1]) + costs[idx][0];
+        dp[1][idx] = Math.min(dp[0][idx - 1], dp[2][idx - 1]) + costs[idx][1];
+        dp[2][idx] = Math.min(dp[0][idx - 1], dp[1][idx - 1]) + costs[idx][2];
+      }
+    }
+
+    int minResult = Math.min(dp[0][costs.length - 1], dp[1][costs.length - 1]);
+    minResult = Math.min(minResult, dp[2][costs.length - 1]);
+
+    return minResult;
+  }
+
+  public static void main(String[] args) {
+    int[][] costs = { { 17, 2, 17 }, { 16, 16, 5 }, { 14, 3, 19 } };
+
+    System.out.println(minCost(costs));
+  }
+
+}
