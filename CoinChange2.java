a9f8ff34753c975cc67560baf14c5284bd717c63
aashutoshtaikar <aashutoshtaikar@gmail.com>
2022-05-14 16:35:01 -0400

    approach comments

--- CoinChange2.java
+++ CoinChange2.java
@@ -3,6 +3,17 @@
  * 
  * time: 2^(n*m) n: no. of coins, m: amount 
  * space: 2^(n*m)
+ * 
+ * No. of all combinations that can make up the amount 
+ * 
+ * Approach:
+ * Use the not choose or choose approach
+ * 1. case1 => count the no. of sub cases if the current denomination not choosen
+ * 2. case2 => count the no. of sub cases if the current denomination choosen
+ * 3. Repeat the above in the recursive tree:
+ *      if the amount at leaf == 0 then return 1 -- valid case
+ *      if the amount < 0 then return 0 -- invalid case as the amount cannot be made up using those coin combinations
+ * 
  */
 class Problem23 {
     public int change(int amount, int[] coins) {
@@ -32,6 +43,29 @@ class Problem23 {
  * 
  * time: O(n*m)
  * space: O(n*m)
+ * 
+ * Approach:
+ * 1. plot the amounts on the x grid (0 to amount)
+ * 2. plot the coin denominations on the y grid ( 0 to coin denominations)
+ * 3. Fill the table such that In how many ways can we make the amount using each denomination
+ * 4. Add a dummy 0 row for simplicity
+ * 5. if the amount is less than the coin denomination 
+ *      then copy same from 1 row above
+ *    else
+ *      the amount from previous row, same column 
+ *          +
+ *      the amount from the current row, going n-steps back from the current column
+ *          (n => current row coin denomination)
+ * 
+ * To reach this approach draw the iterative tree first:
+ * Eg. 
+ * 
+ *   [ 1 , 2 ]
+ *      0 / \ 1
+ *       /   \
+ *      / 0   \    0
+ *     [1]    [1,2]
+ * 
  */
 class Problem23Approach1 {
     public int change(int amount, int[] coins) {
@@ -60,6 +94,21 @@ class Problem23Approach1 {
  * 
  * time: O(n*m)
  * space: O(n)
+ * 
+ * Approach:
+ * Using the same previous approach
+ * To save space we can just use 1D array for storing the dp
+ * because the columns till the coin denomination stay the same (use the same data from 1 row above for resp. columns)
+ * 
+ * Note: 
+ * the overwrite operation in the if condition is unnecessary 
+ *  and is only kept for illustration purpose
+ *  instead we can just have the below:
+ *  
+ *  if(j >= coins[i-1]){
+ *     dp[j] = dp[j] + dp[j-coins[i-1]]; 
+ *  }
+ * 
  */
 class Problem23Approach2 {
     public int change(int amount, int[] coins) {
