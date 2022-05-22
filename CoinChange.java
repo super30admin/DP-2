b1443b298130aaa525b712336f10a2e3069a3a4d
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-05-22 11:28:40 -0500

    Create CoinChange.java

---
+++ CoinChange.java
@@ -0,0 +1,14 @@
+// o(n*amount) time || o(amount)-space
+class Solution {
+  public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
+
+    for (int coin : coins) {
+      for (int x = coin; x < amount + 1; ++x) {
+        dp[x] += dp[x - coin];
+      }
+    }
+    return dp[amount];
+  }
+}
