85738bc1c5bbbca036a7cfa4be7c3072e7a4db78
Damandeep Singh <sdamandeep18@gmail.com>
2022-06-03 11:58:33 -0700

    Completed Sprint DP-2

---
+++ CoinChange2.java
@@ -0,0 +1,21 @@
+//time complexity: O(m*n)
+//space complexity: O(m*n)
+class Solution {
+    public int change(int amount, int[] coins) {
+        int m = coins.length; int n = amount;
+        int [][] dp = new int[m+1][n+1];
+        dp[0][0] = 1;
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                if(j < coins[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                    
+                } else {
+                    dp[i][j] = dp[i -1][j] + dp[i][j - coins[i-1]];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+}
