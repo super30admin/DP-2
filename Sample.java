9d6a6a2717d0e81e10a3f2134e476fa0be8435ce
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-03 13:42:12 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,23 +1,82 @@
-//****COIN CHANGE:  PROBLEM 2: DP table approach****
-// Time Complexity :for n number of coins and m amount:::: O(n*m);
-// Space Complexity : for n number of coins and m amount:::: O(n*m);
-// Did this code successfully run on Leetcode : Y
-// Any problem you faced while coding this :N
+//****PAINT HOUSE: DP APPRAOCH****
+//Time complexity: colors: 3  * rows:m , 
+//therefore it is o(m*3) i.e :  0(m);
+//Space complexity: 0(1);
+//Leetcode runnable: Y;
+
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int m= coins.length;
-        int n=amount;
-        int [][] dp= new int[m+1][n+1];
+    public int minCost(int[][] costs) {
+        if(costs==null || costs.length==0) return 0;
+        int n=costs.length;
         
-        //first row 
-        for(int j=1;j<dp[0].length;j++)
+        for(int i=n-2;i>=0;i--)
         {
-            dp[0][j]=99999;
+            costs[i][0] +=  Math.min(costs[i+1][1], costs[i+1][2]);
+            costs[i][1] +=  Math.min(costs[i+1][0], costs[i+1][2]);
+            costs[i][2] +=  Math.min(costs[i+1][0], costs[i+1][1]);
         }
+        
+        //find min in the top row
+         return Math.min(costs[0][0], Math.min(costs[0][1], costs[0][2]));
+        
+    }
+}
+
+//****PAINT HOUSE: RECURSIVE APPRAOCH****
+class Solution {
+    public int minCost(int[][] costs) {
+        if(costs==null || costs.length== 0) return 0;
+        
+        int caseR=helper(costs, 0, 0, 0 );
+        int caseB=helper(costs, 0, 1, 0 );
+        int caseG=helper(costs, 0, 2, 0 );
+        
+        return Math.min(caseR, Math.min(caseB, caseG));
+        
+        
+    }
+    
+    private int helper(int[][] costs, int r, int color, int mincost)
+    {
+        //Base
+        if(r==costs.length)
+        {
+            return mincost;
+        }
+        
+        //logic 
+        if(color==0)
+        {
+            return Math.min(helper(costs, r+1, 1, mincost+costs[r][0]),helper(costs, r+1, 2,                     mincost+costs[r][0]));
+        }
+        
+        if(color==1)
+        {
+            return Math.min(helper(costs, r+1, 0, mincost+costs[r][1]),helper(costs, r+1, 2,                     mincost+costs[r][1]));
+        }
+                            
+        if(color==2)
+        {
+            return Math.min(helper(costs, r+1, 0, mincost+costs[r][2]),helper(costs, r+1, 1,                     mincost+costs[r][2]));
+        }
+        return 8282;
+    }
+}
+
+//****COIN CHANGE:2 : DP APPRAOCH ****
+//time complexity: o(m*n) : m is amount and n is coins
+//space complexity:0(1)
+//leetcode runnable: Y;
+class Solution {
+    public int change(int amount, int[] coins) {
+        int m= coins.length;
+        int n= amount;
+        int[][] dp = new int[m+1][n+1];
+        dp[0][0]=1;
         
         for(int i=1;i<dp.length;i++)
         {
-            for(int j=1;j<dp[0].length;j++)
+            for(int j=0;j<dp[0].length;j++)
             {
                 if(j<coins[i-1])
                 {
@@ -25,45 +84,43 @@ class Solution {
                 }
                 else
                 {
-                    dp[i][j]=Math.min(dp[i-1][j], dp[i][j-coins[i-1]] +1);
+                    dp[i][j]=dp[i-1][j]+ dp[i][j-coins[i-1]];
                 }
             }
         }
-        
-        if(dp[m][n]>=99999) return -1;
         return dp[m][n];
-        
-    
     }
 }
-//****COIN CHANGE:Recursive approach:: Time limit exceed error****
 
+//****COIN CHANGE:2 RECURSIVE APPROACH
+//time complexity: o(m*n) : m is amount and n is coins
+//space complexity:0(1)
+//leetcode runnable: TLE ERROR
 class Solution {
-    public int coinChange(int[] coins, int amount) {
+    public int change(int amount, int[] coins) {
+        if(coins==null || coins.length==0) return 0;
         
-        return helper(coins, amount, 0 , 0);
+        
+        return helper(amount, coins, 0);
         
     }
     
-    private int helper(int[] coins, int amount, int i, int mincoins)
+    private int  helper(int amount, int[] coins, int i)
     {
-        //BaseCase::
-        if(amount==0) return mincoins;
-        if(amount<0 || i==coins.length) return -1;
+        //Base case
+        if(amount==0) return 1;
+        if(amount<0 || i==coins.length) return 0;
         
-        
-        
-        
-        //Logic::
+        //Logic
         
         //To choose
-        int case1=helper(coins, amount-coins[i], i, mincoins + 1);
+        int case1= helper(amount-coins[i], coins, i);
         
         //Not to choose
-        int case2=helper(coins, amount, i+1, mincoins);
-        if(case1==-1) return case2;
-        if(case2==-1) return case1;
+        int case2=helper(amount, coins, i+1);
         
-        return Math.min(case1, case2);
+        return case1+case2;
+        
     }
+    
 }
