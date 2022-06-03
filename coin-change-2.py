0dd8aa12ff2d5874f50fdbecca30dd7249b693ee
Sandeep Kumar Reddy Onteddy (Student) <sonteddy@sundevils.asu.edu>
2022-06-03 12:17:28 -0700

    Completed DP-2 Please Review

---
+++ coin-change-2.py
@@ -0,0 +1,13 @@
+# Time Complexity: O(n*k)
+# Space Complexity: O(n*k)
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp=[[0 for i in range(amount+1)] for j in range(len(coins)+1)]
+        dp[0][0]=1
+        for i in range(1,len(coins)+1):
+            for j in range(amount+1):
+                if j<coins[i-1]:
+                    dp[i][j]=dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]]
+        return dp[len(coins)][amount]
