f9f1abecb43079db813bb5a370b2f832b3875b24
Vraj Himanshu Reshamdalal <30725458+vraj9845@users.noreply.github.com>
2022-06-05 09:52:29 -0400

    Update Solution.java

--- paint houses/Solution.java
+++ paint houses/Solution.java
@@ -13,10 +13,11 @@ public class Solution {
         for(int i=0;i<costs[0].length;i++){
             dp[i]=0;
         }
+        int dp0=0,dp1=0,dp2=0;
         for(int i==0;i<costs.length;i++){
-            int dp0=costs[i][0] + Math.min(dp1,dp2);
-            int dp2=costs[i][1] + Math.min(dp0,dp3);
-            int dp3=costs[i][2] + Math.min(dp1,dp0);
+             dp0=costs[i][0] + Math.min(dp1,dp2);
+             dp1=costs[i][1] + Math.min(dp0,dp2);
+             dp2=costs[i][2] + Math.min(dp1,dp0);
         }
         int min=dp0;
         for(inti=1;i<dp.length;i++){
