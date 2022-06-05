f2fbcf38ba04df6e540a4fc94cb1627d0e062d5a
Maharshi Kondapaneni <maharshi@ou.edu>
2022-06-05 17:12:30 -0500

    done

---
+++ coinChange2.py
@@ -0,0 +1,36 @@
+class RecursiveSolution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        if not coins:
+            return 0
+        return self.helper(coins, amount, 0)
+
+    def helper(self, coins, amount, idx):
+        if amount == 0:
+            return 1
+        if amount < 0 or idx >= len(coins):
+            return 0
+
+        choose = self.helper(coins, amount - coins[idx], idx)
+        noChoose = self.helper(coins, amount, idx + 1)
+
+        return choose + noChoose
+
+
+class DPSolution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        if not coins:
+            return 0
+        dp = [[0] * (amount + 1) for i in range(len(coins) + 1)]
+
+        dp[0][0] = 1
+        # print(dp)
+        for i in range(1, len(coins) + 1):
+            for j in range((amount + 1)):
+                if coins[i - 1] > j:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]]
+        # print(dp)
+        return dp[-1][-1]
+
+
