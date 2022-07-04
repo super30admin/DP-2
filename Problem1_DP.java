8604d4840a55f570b41ef08b00c0ef49a1f885be
Sreevikram Chandrasekhar <vikramchandra97@gmail.com>
2022-07-04 16:19:14 +0530

    Completed Sprint DP-2

---
+++ Problem1_DP.java
@@ -0,0 +1,34 @@
+//Time Complexity : O(m*n)
+//Space Complexity : O(1)
+//Code run successfully on LeetCode.
+public class Problem1_DP {
+	
+	public int minCost(int[][] costs) {
+       
+		if(costs == null|| costs.length == 0)
+			return -1;
+		
+		int m = costs.length;
+		int n = costs[0].length;
+		
+		for(int i = m-2; i >=0; i--) {
+			for(int j=0; j < n; j++) {
+				
+				if(j == 0) {
+					costs[i][j] = Math.min(costs[i][j] + costs[i+1][j+1], costs[i][j] + costs[i+1][j+2]);
+				}
+				if(j == 1) {
+					costs[i][j] = Math.min(costs[i][j] + costs[i+1][j-1], costs[i][j] + costs[i+1][j+1]);
+				}
+				if(j == 2) {
+					costs[i][j] = Math.min(costs[i][j] + costs[i+1][j-1], costs[i][j] + costs[i+1][j-2]);
+				}
+				
+			}
+		}
+		
+		return Math.min(costs[0][0], Math.min(costs[0][1], costs[0][2]));
+		
+    }
+
+}
