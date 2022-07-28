ff4f6b1c9ab0fedf89c442ab0d38ba166f730ac3
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-07-28 19:22:19 -0400

    Update PaintHouse.java

--- PaintHouse.java
+++ PaintHouse.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
 class Solution {
     public int minCost(int[][] costs) {
         int n =costs.length;
