854e93f0668a7474de865b8a4d8336e1f56fc1ed
altafzr18 <32244460+altafzr18@users.noreply.github.com>
2022-07-29 09:17:13 +0530

    Completed DP2

---
+++ pAINT HOUSE_256.java
@@ -0,0 +1,23 @@
+// Time Complexity :O(MxN)
+// Space Complexity :O(MxN)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+class Solution {
+    public int minCost(int[][] costs) {
+        int n =costs.length;
+        int[][] dp = new int[n][3];
+
+        for(int i=0;i<3;i++){
+            dp[n-1][i] = costs[n-1][i];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            dp[i][0] = costs[i][0] + Math.min(dp[i+1][1],dp[i+1][2]);
+            dp[i][1] = costs[i][1] + Math.min(dp[i+1][0],dp[i+1][2]);
+            dp[i][2] = costs[i][2] + Math.min(dp[i+1][0],dp[i+1][1]);
+        }
+        return Math.min(dp[0][0],Math.min(dp[0][1],dp[0][2]));
+    }
+}
+
+// Your code here along with comments explaining your approach
