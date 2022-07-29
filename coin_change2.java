32ac4d32b1b91b78ad34dd1751b27c2a41b596ea
shefali singh <shefali.singh@keeptruckin.com>
2022-07-28 20:53:26 -0700

    DP2 hw

---
+++ coin_change2.java
@@ -0,0 +1,23 @@
+// Time Complexity :O(m*n)
+// Space Complexity : O(m*n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : no
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int m = coins.length;
+        int n = amount;
+        int[][] dp = new int[m+1][n+1];
+        dp[0][0]=1;
+        for(int i=1;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(coins[i-1]>j){
+                   dp[i][j]=dp[i-1][j]; 
+                }
+                else
+                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+            }
+        }
+        return dp[m][n];
+    }
+}
