96afc6c0e5dd807bb6fb9407f98c509a64d2758b
Saiteja Garlapati <86909787+lostrider07@users.noreply.github.com>
2022-07-31 19:36:44 -0700

    Coin change 2

---
+++ coin-change-2.py
@@ -0,0 +1,17 @@
+# Time complexity : O(m*n)
+#  Space complexity: O(m*n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
+        dp[0] = [1] * (len(coins) + 1)
+        
+        for a in range(1, amount + 1):
+            for i in range(len(coins) -1, -1 ,-1):
+                dp[a][i] = dp[a][i+1]
+                if a - coins[i] >= 0:
+                    dp[a][i] += dp[a - coins[i]][i]
+        return dp[amount][0]
