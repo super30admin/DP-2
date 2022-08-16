62958881448f9867bd4aced79045aea0afd2cd2c
Darshil Shah <darshilshah1899@gmail.com>
2022-08-16 17:26:19 -0400

    DP 2
    DP 2

---
+++ Coin Change 2.java
@@ -0,0 +1,16 @@
+// Time Complexity : O(N×amount)
+// Space Complexity : O(amount)
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int x = coin; x < amount + 1; ++x) {
+                dp[x] += dp[x - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
