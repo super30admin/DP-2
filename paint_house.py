5a63280c29a962cce57a0501342fb316b4d11cef
Siddhant Shah <siddhanta.shah@st.niituniversity.in>
2022-08-29 13:29:47 -0700

    Modifies paint house

--- paint_house.py
+++ paint_house.py
@@ -2,9 +2,9 @@ class Solution:
     def minCost(self,costs):
         dp=[0,0,0]
         for i in range(len(costs)):
-            dp0=dp[i][0] + min(dp[i][1],dp[i][2])
-            dp1=dp[i][1] + min(dp[i][0],dp[i][2])
-            dp2=dp[i][2] + min(dp[i][0],dp[i][1])
+            dp0=costs[i][0] + min(dp[1],dp[2])
+            dp1=costs[i][1] + min(dp[0],dp[2])
+            dp2=costs[i][2] + min(dp[0],dp[1])
 
             dp=[dp0,dp1,dp2]
         
