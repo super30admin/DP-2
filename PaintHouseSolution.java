8192d7699084e1475674d897bad393d2672aad20
Sumanth Shankar <sumanth@rezy360.com>
2022-09-23 12:17:05 -0700

    Solution for coin change 2

--- PaintHouseSolution.java
+++ PaintHouseSolution.java
@@ -4,26 +4,4 @@
 // Any problem you faced while coding this : No
 
 public class PaintHouseSolution {
-    public int change(int amount, int[] coins) {
-        int m = coins.length;
-        int n = amount;
-        
-        int[][] dp = new int[m + 1][n + 1];
-        
-        for(int i = 0; i < m + 1; i += 1) {
-            dp[i][0] = 1;
-        }
-        
-        for(int i = 1; i < m + 1; i += 1) {
-            for(int j = 1; j < n + 1; j += 1) {
-                if(j < coins[i - 1]) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-                }
-            }
-        }
-        
-        return dp[m][n];
-    }
 }
