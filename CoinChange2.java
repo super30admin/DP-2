cfc196ae21f679fa2635ff93661c94c78b4f267a
SnehaNellutla <snehan844@gmail.com>
2022-10-13 00:18:58 -0400

    done

---
+++ CoinChange2.java
@@ -0,0 +1,33 @@
+
+// Time Complexity : O(m*n) 
+// Space Complexity : O(N) 
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+class Solution {
+    public int change(int amount, int[] coins) {
+         if(coins.length==0 || amount==0) return 1;
+        int dp[][]=new int[coins.length+1][amount+1];
+        
+        for(int i=0;i<coins.length+1;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<amount+1;i++){
+            dp[0][i]=0;
+        }
+        
+        
+        for(int i=1;i<coins.length+1;i++){
+            for(int j=1;j<amount+1;j++){
+                if(j<coins[i-1]){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+                }
+            }
+        }
+        
+        return dp[coins.length][amount];
+    }
+}
