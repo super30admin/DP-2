fde9327a3b9e827b76b4fef2a2ad19696d862e4b
eshwar2807 <114554105+eshwar2807@users.noreply.github.com>
2022-10-15 20:37:31 -0700

    Create Exe_1.java

---
+++ Exe_1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minCost(int[][] costs) {
+        for(int i = 1; i<costs.length; ++i){
+            costs[i][0] += Math.min(costs[i-1][1],costs[i-1][2]);
+            costs[i][1] += Math.min(costs[i-1][0],costs[i-1][2]);
+            costs[i][2] += Math.min(costs[i-1][0],costs[i-1][1]);
+        }
+    return Math.min(costs[costs.length-1][0],Math.min(costs[costs.length-1][1],costs[costs.length-1][2]));}
+    
+}
+//tc=O(n)
+//sc=O(1)
