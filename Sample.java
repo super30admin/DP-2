1b5f38a9fa3b88bde047b733fac1e8954ed3da5d
Riddhi Jaju <riddhijaju2610@gmail.com>
2022-11-22 20:11:27 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,7 +3,7 @@ Problem-518 (coin change 2)
 // Time Complexity :O(mn)
 // Space Complexity :O(mn)
 // Did this code successfully run on Leetcode :Yes
-// Any problem you faced while coding this :Yes
+// Any problem you faced while coding this :No
 
 
 // Your code here along with comments explaining your approach
@@ -31,5 +31,39 @@ Problem-518 (coin change 2)
             }
         }
         return dp[coins.length][amount];
+    }
+}
+
+
+
+
+
+
+
+
+Problem-256 (Paint-house)
+// Time Complexity :O(mn)
+// Space Complexity :O(mn)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
+
+
+// Your code here along with comments explaining your approach
+  
+  class Solution {
+    public int minCost(int[][] costs) {
+        int n=costs.length;
+        int min=Integer.MAX_VALUE;
+        for(int i=n-2; i>=0;i--)
+        {
+            costs[i][0]= costs[i][0]+ Math.min(costs[i+1][1], costs[i+1][2]);
+            costs[i][1]= costs[i][1]+ Math.min(costs[i+1][0], costs[i+1][2]);
+            costs[i][2]= costs[i][2]+ Math.min(costs[i+1][0], costs[i+1][1]);
+        }
+        for(int i=0; i<=2;i++)
+        {
+            min= Math.min(costs[0][i], min);
+        }
+        return min;
     }
 }
