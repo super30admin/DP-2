cfbe1e9825ce7f6452e4fb4c5ec23676edad79d0
sivakami-projects <126191544+sivakami-projects@users.noreply.github.com>
2023-03-08 21:27:16 -0800

    Update paintHouse.java

--- paintHouse.java
+++ paintHouse.java
@@ -1,20 +1,28 @@
-/**
-Time Complexity - O(N) where N is the size of the nums array.
+/*
+Time Complexity - O(N) where is the number of houses.
 Space Complexity - O(1)
- */
+*/
 class Solution {
-    public int rob(int[] nums) {
-        if(nums == null || nums.length == 0) return 0;
-    
-        int oldPrev = 0;
-        int newPrev = nums[0];
-
-        for(int i = 1; i < nums.length; i++) {
-            int temp = newPrev;
-            newPrev = Math.max(newPrev, oldPrev + nums[i]);
-            oldPrev = temp;
+    public int minCost(int[][] costs) {
+     
+        if(costs == null || costs.length == 0)
+            return 0;
+        
+        int R = costs[0][0];
+        int G = costs[0][1];
+        int B = costs[0][2];
+        
+        for(int i = 1; i < costs.length; i++) 
+        {
+            int tempR = Math.min(G, B) + costs[i][0];
+            int tempG = Math.min(R, B) + costs[i][1];
+            int tempB = Math.min(R, G) + costs[i][2];
+            
+            R = tempR;
+            G = tempG;
+            B = tempB;  
         }
-
-        return newPrev;
+        
+     return Math.min(Math.min(R,G),B) ;  
     }
 }
