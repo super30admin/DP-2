278ac8d3955757b16e777ff11cd70f436548f600
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-03-19 20:00:06 -0700

    Update PaintHouse.java

--- src/PaintHouse.java
+++ src/PaintHouse.java
@@ -35,31 +35,46 @@ class Paint
 
         return minVal;
 
-// ***************************** Bottom-Up Approach *****************************
-//        int[] prev = costs[0];
-//        for(int i=1; i<costs.length; i++)
-//        {
-//            int[] cur = costs[i];
-//            for(int j=0; j<costs[0].length; j++)
-//            {
-//                int min = Integer.MAX_VALUE;
-//                for(int k=0; k<costs[0].length; k++)
-//                {
-//                    if(k != j)                                        // Not considering previous color for current house
-//                    {
-//                        min = Math.min(min,prev[k]);
-//                    }
-//                }
-//                cur[j] = min + cur[j];                                // Finding minimum cost up to current house
-//            }
-//            prev = cur ;
-//        }
-//
-//        int minVal = prev[0];                                         // Getting minimum amount at the end
-//        for(int cost : prev)
-//            minVal = Math.min(cost,minVal) ;
-//
-//        return minVal;
+// ***************************** Bottom-Up Approach with 5 extra variables only *****************************
+//         int n = costs.length;
+//         int colorR = costs[n-1][0];
+//         int colorB = costs[n-1][1];
+//         int colorG = costs[n-1][2];
+        
+//         for(int n-2; i>=0; i--) {
+//             int tempR = colorR;
+//             int tempB = colorB;
+//             colorR = costs[i][0] + Math.min(colorB, colorG);
+//             colorB = costs[i][1] + Math.min(colorR, colorG);
+//             colorG = costs[i][2] + Math.min(colorR, colorB);
+//         }
+        
+//         return Math.min(colorR, Math.min(colorB, colorG));
+// // ***************************** Bottom-Up Approach *****************************
+// //        int[] prev = costs[0];
+// //        for(int i=1; i<costs.length; i++)
+// //        {
+// //            int[] cur = costs[i];
+// //            for(int j=0; j<costs[0].length; j++)
+// //            {
+// //                int min = Integer.MAX_VALUE;
+// //                for(int k=0; k<costs[0].length; k++)
+// //                {
+// //                    if(k != j)                                        // Not considering previous color for current house
+// //                    {
+// //                        min = Math.min(min,prev[k]);
+// //                    }
+// //                }
+// //                cur[j] = min + cur[j];                                // Finding minimum cost up to current house
+// //            }
+// //            prev = cur ;
+// //        }
+
+// //        int minVal = prev[0];                                         // Getting minimum amount at the end
+// //        for(int cost : prev)
+// //            minVal = Math.min(cost,minVal) ;
+
+// //        return minVal;
     }
 
     private int minCost(int[][] costs, int index, int prevColor)
