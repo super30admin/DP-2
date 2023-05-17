3aacb9d0d644a0c63b171277f83d97f7a5201210
lohitb009 <45014992+lohitb009@users.noreply.github.com>
2023-05-16 18:15:36 -0700

    Submission in C#
    Submission in C#

---
+++ ClassPaintHouses.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCodeSubmission.DP2;
+
+public class ClassPaintHouses
+{
+    public int MinCost(int[][] costs) {
+        
+        // mutate the original matrix
+        int rows = costs.GetLength(0);
+        int cols = costs[0].GetLength(0);
+
+        for (int r = rows - 2; r > -1; r--)
+        {
+            for (int c = cols - 1; c > -1; c--)
+            {
+                // cols will be fixed to 3, rows can be n
+                if (c == 2)
+                {
+                    costs[r][c] += Math.Min(costs[r + 1][c - 1], costs[r + 1][c - 2]);
+                }
+                else if (c == 1)
+                {
+                    costs[r][c] += Math.Min(costs[r + 1][c - 1], costs[r + 1][c + 1]);
+                }
+                else if (c == 0)
+                {
+                    costs[r][c] += Math.Min(costs[r + 1][c + 1], costs[r + 1][c + 2]);
+                }
+            }
+        }
+        
+        // print the mutated matrix
+        /*for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                Console.Write(costs[r][c]);
+                Console.Write("\t");
+            }
+            Console.WriteLine();
+        }*/
+
+        // find the minimum in first row 
+        int minValue = int.MaxValue;
+        for (int i = 0; i < cols; i++)
+        {
+            minValue = Math.Min(minValue, costs[0][i]);
+        }
+        
+        return minValue;
+    }
+}
---
+++ CoinChange2.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeSubmission.DP2;
+
+public class CoinChange2
+{
+    /*
+     * To make amount 0 with 0-denomination coin, I need 1 coin of 0 denomination, thus value is 1
+     */
+
+    public int Memorization2D(int amount, int[] coins)
+    {
+        int[,] memorization2D = new int[coins.GetLength(0)+1,amount+1];
+        memorization2D[0, 0] = 1;
+
+        for (int r = 1; r < memorization2D.GetLength(0); r++)
+        {
+            int denomination = coins[r - 1];
+            for (int c = 0; c < memorization2D.GetLength(1); c++)
+            {
+                // copy everything from above if coin-denomination is greater than amount i.e. c
+                if (denomination > c)
+                {
+                    memorization2D[r, c] = memorization2D[r - 1, c];
+                }
+                else
+                {
+                    memorization2D[r, c] = memorization2D[r - 1, c] + memorization2D[r, c - denomination];
+                }
+            }
+        }
+        
+        //print the memorization2D matrix
+        /*for (int r = 0; r < memorization2D.GetLength(0); r++)
+        {
+            for (int c = 0; c < memorization2D.GetLength(1); c++)
+            {
+                Console.Write(memorization2D[r,c]);
+                Console.Write("\t");
+            }
+            Console.WriteLine();
+        }*/
+
+        // return the result
+        return memorization2D[memorization2D.GetLength(0) - 1, memorization2D.GetLength(1) - 1];
+    }
+    
+    public int Change(int amount, int[] coins)
+    {
+        return this.Memorization2D(amount, coins);
+    }
+}
