6d3705c838c554e9abbcc3309024a76037863fb7
jackpraveenraj <jackpraveenraj.work@gmail.com>
2023-06-05 15:04:28 -0700

    problems completed

--- problem1.cpp
+++ problem1.cpp
@@ -2,3 +2,25 @@
 # Space Complexity : O(n)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
+
+class Solution
+{
+    public:
+    int minCost(std::vector<std::vector<int>> &costs)
+    {
+        int N = costs.size();
+        int r = costs[0][0];
+        int b = costs[0][1];
+        int g = costs[0][2];
+        for (int i = 1; i < N; i++)
+        {
+            int prevR = r;
+            int prevB = b;
+            int prevG = g;
+            r = min(prevB,prevG) + costs[i][0];
+            b = min(prevR,prevG) + costs[i][1];
+            g = min(prevR,prevB) + costs[i][2];
+        }
+        return min(r,min(b,g));
+    }
+};
