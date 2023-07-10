364640da7dd10956cf74cd4490dfdbf56f9e7866
iamshubham28 <shubham.ashtekar28@gmail.com>
2023-07-10 15:00:51 +0530

    Completed Sprint DP-2

---
+++ paint_house.py
@@ -0,0 +1,11 @@
+
+def min_cost(self, costs: List[List[int]]) -> int:
+    dp=[0,0,0]
+    for i in range(len(costs)):
+        dp0= costs[i][0]+ min(dp[1], dp[2])
+        dp1= costs[i][1]+ min(dp[0], dp[2])
+        dp2= costs[i][2]+ min(dp[0], dp[1])
+        dp= [dp0, dp1, dp2]
+
+    return min(dp)
+    
