3eceb6075d4975f723ddf782c2d29ac5b556f541
Priyanshu Shrivastava <109136206+Spriy4nshu@users.noreply.github.com>
2023-07-13 20:49:55 -0400

    DP-2 complete

---
+++ coin-change-2-dp.cpp
@@ -0,0 +1,23 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for (int c : coins) {
+            for (int i = c; i <= amount; i++) {
+                dp[i] += dp[i - c];
+            }
+        }
+
+        return dp[amount];
+    }
+};
