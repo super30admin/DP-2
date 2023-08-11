1f7387d875c3fe6deab3932480cac22420c181d4
chsrimanaswi <53294091+chsrimanaswi@users.noreply.github.com>
2023-08-11 13:58:46 -0400

    Update PaintHouse.java

--- PaintHouse.java
+++ PaintHouse.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(mn)
+// Time Complexity : O(n)
 // Space Complexity : O(mn)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this :No
