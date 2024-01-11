0b38ba77178b451ec319d639d9de2e4bd5fe64f4
Harsh Gupta <harshhgupta16@gmail.com>
2024-01-11 15:39:10 -0500

    added efficient sol of coin change 2

--- coinChange2.java
+++ coinChange2.java
@@ -1,41 +1,68 @@
 // Time Complexity : O(m*n)
-// Space Complexity : O(m*n)
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 // Your code here along with comments explaining your approach
+// Using bottom up approach and used 1D array for space optimization
 
 class Solution {
-    public void helper(int amount, int[] coins, int[][] dp) {
+
+    private void helper(int amount, int[] coins, int[] dp) {
         int m = coins.length;
         int n = amount;
+
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (j < coins[i - 1]) {
-                    dp[i][j] = dp[i - 1][j];
+                    dp[j] = dp[j];
                 } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                    dp[j] = dp[j] + dp[j - coins[i - 1]];
                 }
             }
         }
-
     }
 
     public int change(int amount, int[] coins) {
-        int m = coins.length;
         int n = amount;
-        int[][] dp = new int[m + 1][n + 1];
-        dp[0][0] = 0;
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int i = 1; i <= n; i++) {
-            dp[0][i] = 0;
-        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
         helper(amount, coins, dp);
-        return dp[m][n];
+        return dp[n];
     }
 }
+
+/*
+ * public void helper(int amount, int[] coins, int[][] dp) {
+ * int m = coins.length;
+ * int n = amount;
+ * for (int i = 1; i <= m; i++) {
+ * for (int j = 1; j <= n; j++) {
+ * if (j < coins[i - 1]) {
+ * dp[i][j] = dp[i - 1][j];
+ * } else {
+ * dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+ * }
+ * }
+ * }
+ * 
+ * }
+ * 
+ * public int change(int amount, int[] coins) {
+ * int m = coins.length;
+ * int n = amount;
+ * int[][] dp = new int[m + 1][n + 1];
+ * dp[0][0] = 0;
+ * for (int i = 1; i <= m; i++) {
+ * dp[i][0] = 1;
+ * }
+ * for (int i = 1; i <= n; i++) {
+ * dp[0][i] = 0;
+ * }
+ * helper(amount, coins, dp);
+ * return dp[m][n];
+ * }
+ */
 
 /*
  * Recursive solution gives TLE
