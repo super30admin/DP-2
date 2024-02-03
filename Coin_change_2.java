62bfec8ecc310f3d7a9a869dfcbada02b53d48e4
SINDHURA_PARUCHURI <paruchuri.sindhu13@gmail.com>
2024-02-03 11:34:18 -0700

    Completed DP-2

---
+++ Coin_change_2.java
@@ -0,0 +1,19 @@
+//TimeComplexity : O(m*n)
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        
+        int [][]dp = new int [coins.length+1][amount+1];
+        dp[0][0] = 1;
+        for(int i =1; i<=coins.length;i++) {
+            for(int j = 0;j<=amount;j++) {
+                  if(j < coins[i-1]) {
+                      dp[i][j] = dp[i-1][j];
+                  } else {
+                      dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                  }
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
