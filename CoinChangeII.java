1f16ba6d593c3c3e75de7ad487684f6c0c2432ad
AryanReddyy <aryan02reddy@gmail.com>
2024-03-20 12:54:47 -0400

    DP - 2

---
+++ CoinChangeII.java
@@ -0,0 +1,25 @@
+// Time Complexity : O(n*amount)
+// Space Complexity : O(n*amount)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class CoinChangeII {
+    public int change(int amount, int[] coins) {
+        int m = coins.length;
+        int n = amount;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (j >= coins[i-1]) {
+                    dp[j] = dp[j] + dp[j-coins[i-1]];
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
