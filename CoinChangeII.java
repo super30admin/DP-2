ac3cd9ee3b818ac38760392999b77f4e51f1e180
Sourav Walke <sourav.wal@gmail.com>
2024-03-26 10:22:55 -0700

    Completed DP-2

---
+++ CoinChangeII.java
@@ -0,0 +1,22 @@
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+public class CoinChangeII {
+
+    public int change(int amount, int[] coins) {
+        int m = coins.length;
+        int n = amount;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (j >= coins[i - 1]) {
+                    dp[j] = dp[j] + dp[j - coins[i - 1]];
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
